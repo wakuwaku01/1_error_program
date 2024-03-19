39add871c90a2c938e7e15a0b9e03f975c370c1a
wakuwaku01 <wakuwaku.post.mail@gmail.com>
2024-03-19 19:30:42 +0300

    first commit

---
+++ 1_error_program/error_program.cpp
@@ -0,0 +1,27 @@
+﻿//homework_1: program_with_errors
+
+#include <iostream>
+
+struct Point {
+private:
+	double m_x;
+	double m_y;
+public:
+	Point(double x, double y) {
+		m_x = x;
+		m_y = y;
+	}
+	void print_point(const Point& point_object) {
+		std::cout << "x:" << point_object.m_x << ", y: " << point_object.m_y << std::endl;
+	}
+};
+
+int main(int argc, char** argv) {
+
+	for (int i = 0; i < 5; i++) {
+		Point my_point(i, 2 * i);
+		my_point.print_point(my_point);
+	}
+	return 0;
+}
+
